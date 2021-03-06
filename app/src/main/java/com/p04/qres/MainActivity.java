c858a1fb959f8b8d223a6ff59cee5eee56491e8b
SatyaSnehith <nsssnehit76@gmail.com>
2021-03-06 23:17:39 +0530

    commit

---
+++ app/src/main/java/com/p04/qres/MainActivity.java
@@ -0,0 +1,14 @@
+package com.p04.qres;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
